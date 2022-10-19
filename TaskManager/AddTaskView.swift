c0dbe93f3d58572d64ba439cc6d2c302ffdd78c5
Tyler Reed <tylerreedytlearning@gmail.com>
2022-10-19 15:34:26 -0400

    modified to go to home tab when saving

--- TaskManager/AddTaskView.swift
+++ TaskManager/AddTaskView.swift
@@ -9,50 +9,37 @@ import SwiftUI
 
 struct AddTaskView: View {
     @ObservedObject var taskModel: TaskViewModel
+    @Binding var selectedTab: TabBarItem
+    
     @Environment(\.dismiss) var dismiss
     @Environment(\.managedObjectContext) private var viewContext
     
     var body: some View {
         VStack {
-            Text("Add Task")
-                .overlay {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image(systemName: "arrow.left")
-                            .font(.title3)
-                            .foregroundColor(.black)
-                    }
+            Form {
+                Section(header: Text("Task Information")) {
+                    TextField("Task Title", text: $taskModel.taskTitle)
                 }
-            
-            VStack(alignment: .leading, spacing: 12) {
-                Text("Task Title")
-                    .font(.caption)
-                    .foregroundColor(.gray)
-                TextField("", text: $taskModel.taskTitle)
-                    .frame(maxWidth: .infinity)
-                    .padding(.top, 8)
             }
             
-            Divider()
-            
             Button {
-                taskModel.addTask()
-                dismiss()
+                withAnimation(.easeInOut) {
+                    taskModel.addTask()
+                    selectedTab = .home
+                }
             } label: {
                 Text("Save Task")
                     .font(.callout)
                     .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical)
                     .foregroundColor(.white)
-                    .background {
-                        Capsule()
-                            .fill(.green)
-                    }
              }
-            .frame(maxHeight: .infinity, alignment: .bottom)
-            .padding(.bottom, 10)
+            .padding(20)
+            .background {
+                Capsule()
+                    .fill(.green)
+                    .shadow(color: .black, radius: 5, x: 0, y: 5)
+            }
+            
             .disabled(taskModel.taskTitle == "")
             .opacity(taskModel.taskTitle == "" ? 0.6 : 1.0)
         }

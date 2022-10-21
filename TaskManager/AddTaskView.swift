6a524383186ecb4f1f7afd1f528f819280163317
Tyler Reed <tylerreedytlearning@gmail.com>
2022-10-21 11:57:21 -0400

    added more properties and modified the whole view

--- TaskManager/AddTaskView.swift
+++ TaskManager/AddTaskView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct AddTaskView: View {
     @ObservedObject var taskModel: TaskViewModel
@@ -15,33 +16,144 @@ struct AddTaskView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     var body: some View {
-        VStack {
-            Form {
-                Section(header: Text("Task Information")) {
-                    TextField("Task Title", text: $taskModel.taskTitle)
+        VStack(spacing: 15) {
+            TextField("Task Title", text: $taskModel.taskTitle)
+                .padding(.horizontal)
+                .padding(.vertical, 10)
+                .background(Color("CardColor").opacity(0.4), in:
+                    RoundedRectangle(cornerRadius: 6, style: .continuous)
+                )
+            
+            TextField("Task Description (Optional)", text: $taskModel.taskDescription)
+                .padding(.horizontal)
+                .padding(.vertical, 10)
+                .background(Color("CardColor").opacity(0.4), in:
+                    RoundedRectangle(cornerRadius: 6, style: .continuous)
+                )
+            
+            
+            HStack(spacing: 0) {
+                ForEach(1...7, id: \.self) { index in
+                    let color = "Card-\(index)"
+                    Circle()
+                        .fill(Color(color))
+                        .frame(width: 30, height: 30)
+                        .overlay {
+                            if color == taskModel.habitColor {
+                                Image(systemName: "checkmark")
+                                    .font(.caption.bold())
+                            }
+                        }
+                        .onTapGesture {
+                            withAnimation {
+                                taskModel.habitColor = color
+                            }
+                        }
+                        .frame(maxWidth: .infinity)
                 }
             }
             
-            Button {
-                withAnimation(.easeInOut) {
-                    taskModel.addTask()
-                    selectedTab = .home
-                }
-            } label: {
-                Text("Save Task")
-                    .font(.callout)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-             }
-            .padding(20)
-            .background {
-                Capsule()
-                    .fill(.green)
-                    .shadow(color: .black, radius: 5, x: 0, y: 5)
-            }
+            Divider()
+                .padding(.vertical, 10)
             
-            .disabled(taskModel.taskTitle == "")
-            .opacity(taskModel.taskTitle == "" ? 0.6 : 1.0)
+            VStack(alignment: .leading, spacing: 6) {
+                Text("Days")
+                    .font(.callout.bold())
+                let weekDays = Calendar.current.weekdaySymbols
+                
+                HStack(spacing: 10) {
+                    ForEach(weekDays, id: \.self) { day in
+                        let index = taskModel.weekDays.firstIndex { value in
+                            return value == day
+                        } ?? -1
+                        Text(day.prefix(2))
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 12)
+                            .background {
+                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    .fill(index != -1 ? Color(taskModel.habitColor) : .gray.opacity(0.4))
+                            }
+                            .onTapGesture {
+                                withAnimation {
+                                    if index != -1 {
+                                        taskModel.weekDays.remove(at: index)
+                                    } else {
+                                        taskModel.weekDays.append(day)
+                                    }
+                                }
+                            }
+                    }
+                }
+                .padding()
+                
+                Divider()
+                
+                HStack {
+                    Text("Per Day:")
+                        .fontWeight(.semibold)
+                    
+                    TextField("", value: $taskModel.frequency, formatter: NumberFormatter())
+                    Stepper("", value: $taskModel.frequency, in: 0...100)
+                }
+                
+                Divider()
+                    .padding(.vertical, 10)
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text("Reminder")
+                            .fontWeight(.semibold)
+                        
+                        Text("Need Notified?")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    DatePicker("", selection: $taskModel.reminderTime, displayedComponents: [.hourAndMinute])
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $taskModel.hasReminder) {
+                        Text("")
+                    }
+                }
+                .opacity(taskModel.notificationAccess ? 1 : 0)
+                
+                Spacer()
+                
+                HStack {
+                    Button {
+                        taskModel.addTask()
+                        
+                        withAnimation {
+                            selectedTab = .home
+                        }
+                    } label: {
+                        Text("Save")
+                            .font(.system(size: 18))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    .frame(width: 75, height: 75)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .background {
+                        Circle()
+                            .fill(.green)
+                            .shadow(color: .black, radius: 2, x: 0, y: 3)
+                    }
+                    .disabled(taskModel.taskTitle == "")
+                    .opacity(taskModel.taskTitle == "" ? 0.4 : 1.0)
+                }
+                .padding(.vertical, 10)
+                
+            }
         }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding()
+        
+                
     }
 }
